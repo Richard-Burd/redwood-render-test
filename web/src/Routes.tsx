fa4c9c058a4f538a9a3c52f65bb41515a5fd5360
richardburd <rick.a.burd@gmail.com>
2023-10-04 15:25:57 -0700

    homepage starter

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -12,6 +12,8 @@ import { Router, Route } from '@redwoodjs/router'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/my-page" page={MyPagePage} name="myPage" />
+      <Route path="/" page={MyPagePage} name="home" />
       <Route notfound page={NotFoundPage} />
     </Router>
   )
